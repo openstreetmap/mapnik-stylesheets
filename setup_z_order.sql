2faa98eb98bde64d11e76ebace34da5dfe519e70
artem <artem@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-05-01 08:39:30 +0000

    fixed layering problem with parking symbols NOTE: requires running new setup_z_order.sql
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@2698 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- setup_z_order.sql
+++ setup_z_order.sql
@@ -12,7 +12,9 @@ update planet_osm_line set z_order= 10*int4(layer) + 3 where highway='residentia
 update planet_osm_line set z_order= z_order + 10  where bridge = 'true';
 update planet_osm_line set z_order= z_order + 10  where bridge = 'yes';
 commit;
-
+begin;
+insert into planet_osm_point(osm_id,name,amenity,way) select osm_id,name,amenity,centroid(way) from planet_osm_polygon where amenity='parking';
+end;
 delete from geometry_columns where f_table_name='planet_osm_roads';
 drop table planet_osm_roads ;
 create table planet_osm_roads as select * from planet_osm_line where highway='motorway' or highway='motorway_link' or highway='trunk' or highway='trunk_link' or highway='primary' or highway='primary_link' or highway='secondary' or highway='secondary_trunk' or char_length(railway) > 0;
