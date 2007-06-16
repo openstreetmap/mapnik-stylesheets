3e868722c66891dd95fcbd786c88c55ec8d7aabb
jonb <jonb@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-06-16 20:06:27 +0000

    generate_image.py: Script for generating a large mapnik image
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@3244 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ generate_image.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#
+# Generates a single large PNG image for a UK bounding box
+# Tweak the lat/lon bounding box (ll) and image dimensions
+# to get an image of arbitrary size.
+#
+# To use this script you must first have installed mapnik
+# and imported a planet file into a Postgres DB using
+# osm2pgsql.
+#
+# Note that mapnik renders data differently depending on
+# the size of image. More detail appears as the image size
+# increases but note that the text is rendered at a constant
+# pixel size so will appear smaller on a large image.
+
+from mapnik import *
+
+if __name__ == "__main__":
+    mapfile = "osm.xml"
+    tile_uri = "image.png"
+    ll = (-6.5,  49.5,   2.1,59)
+    z = 10
+    imgx = 500 * z
+    imgy = 1000 * z
+
+    m = Map(imgx,imgy)
+    load_map(m,mapfile)
+    prj = Projection("+proj=merc +datum=WGS84")
+    c0 = prj.forward(Coord(ll[0],ll[1]))
+    c1 = prj.forward(Coord(ll[2],ll[3]))
+    bbox = Envelope(c0.x,c0.y,c1.x,c1.y)
+    m.zoom_to_box(bbox)
+    im = Image(imgx,imgy)
+    render(m, im)
+    view = im.view(0,0,imgx,imgy) # x,y,width,height
+    save_to_file(tile_uri,'png',view)
