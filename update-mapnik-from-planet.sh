0ed4cddd1f70f697f0436d43f890037ce59a5687
joerg <joerg@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-09-30 21:49:28 +0000

    update-mapnik-from-planet.sh: use option -q for quiet, use user_name for creation of user too
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@4782 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- update-mapnik-from-planet.sh
+++ update-mapnik-from-planet.sh
@@ -13,53 +13,44 @@ export user_name="osm"
 export database_name="gis"
 
 
-echo "check if we already have an osm user"
+echo "check if we already have an user '$user_name'"
 
-if ! id osm ; then
-    echo "create osm User"
-    useradd osm
+if ! id "$user_name" ; then
+    echo "create '$user_name' User"
+    useradd "$user_name"
 fi
 
-mkdir -p /home/osm/osm/planet
-chown -R osm /home/osm
-chmod -R +rwX /home/osm
+mkdir -p "/home/$user_name/osm/planet"
+chown -R "$user_name" "/home/$user_name"
+chmod -R +rwX "/home/$user_name"
 
 
 echo "mirroring planet File"
-if ! sudo -u osm osm-planet-mirror -v -v --planet-dir=$planet_dir ; then 
+if ! sudo -u "$user_name" osm-planet-mirror -v -v --planet-dir=$planet_dir ; then 
     echo "Cannot Mirror Planet File"
     exit
 fi
 
-echo "------- Drop complete Database"	
+echo "------- Drop complete Database '$database_name'"
 sudo -u postgres dropdb -Upostgres   "$database_name"
 sudo -u postgres dropuser -Upostgres "$user_name"
 
 echo
-echo "# ----------- Create Database"
-sudo -u postgres createuser -Upostgres -S -D -R "$user_name"  || exit -1 
-sudo -u postgres createdb -Upostgres  -EUTF8 "$database_name"  || exit -1 
+echo "# ----------- Create Database '$database_name'"
+sudo -u postgres createuser -Upostgres  -q -S -D -R "$user_name"  || exit -1 
+sudo -u postgres createdb -Upostgres  -q  -EUTF8 "$database_name"  || exit -1 
 sudo -u postgres createlang plpgsql "$database_name"  || exit -1 
-sudo -u postgres psql -Upostgres "$database_name" </usr/share/postgresql-8.2-postgis/lwpostgis.sql \
-    2>&1 | grep -v -E \
-    -e '^CREATE AGGREGATE$' \
-    -e '^CREATE FUNCTION$' \
-    -e '^CREATE OPERATOR CLASS$' \
-    -e '^CREATE OPERATOR$' \
-    -e '^UPDATE 1$' \
-    -e '^CREATE CAST$' \
-    -e '^COMMIT$' \
-    -e '^CREATE TYPE$'
+sudo -u postgres psql -q -Upostgres "$database_name" </usr/share/postgresql-8.2-postgis/lwpostgis.sql 
 
 echo 
-echo "# ----------- Create Database and Grant rights"
+echo "# ----------- Grant rights on Database '$database_name' for '$user_name'"
 (
     echo "GRANT ALL ON SCHEMA PUBLIC TO \"$user_name\";" 
     echo "GRANT ALL on geometry_columns TO \"$user_name\";"
     echo "GRANT ALL on spatial_ref_sys TO \"$user_name\";" 
     echo "GRANT ALL ON SCHEMA PUBLIC TO \"$user_name\";" 
-) | sudo -u postgres psql -Upostgres "$database_name"
+) | sudo -u postgres psql -q -Upostgres "$database_name"
 
 echo ""
 echo "--------- Unpack and import $planet_file"
-sudo -u osm osm2pgsql $planet_file
+sudo -u "$user_name" osm2pgsql $planet_file
