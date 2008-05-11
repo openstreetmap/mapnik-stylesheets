6d3470467fd3b5f0c53c25d7972a23d3a51d1ff9
joerg <joerg@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-05-11 08:53:17 +0000

    openstreetmap-mapnik-world-boundaries/debian/update_world_boundaries_dir.sh: remove duplicate subdirectory
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@7750 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- openstreetmap-mapnik-world-boundaries/debian/update_world_boundaries_dir.sh
+++ openstreetmap-mapnik-world-boundaries/debian/update_world_boundaries_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-instal_dir=debian/openstreetmap-mapnik-world-boundaries/usr/share/mapnik/world_boundaries
+install_dir=debian/openstreetmap-mapnik-world-boundaries/usr/share/mapnik
 
 # remove old files
 rm -rf debian/openstreetmap-mapnik-world-boundaries/usr/share/mapnik/world_boundaries
@@ -30,25 +30,25 @@ if ! echo "$@" | grep "no-mirror" ; then
 	http://hypercube.telascience.org/~kleptog/processed_p.zip
 fi
 
-mkdir -p ${instal_dir}/world_boundaries
+mkdir -p ${install_dir}/world_boundaries
 
 echo "Unpack world_boundaries-spherical"
 tar -xvz \
-    --directory=${instal_dir}/ \
+    --directory=${install_dir}/ \
     -f mirror/world_boundaries-spherical.tgz
 
 echo "Unpack processed_p.zip"
-unzip mirror/processed_p.zip -d ${instal_dir}/world_boundaries
+unzip mirror/processed_p.zip -d ${install_dir}/world_boundaries
 
-mv ${instal_dir}/world_boundaries/coastlines/*  ${instal_dir}/world_boundaries/
-rmdir  ${instal_dir}/world_boundaries/coastlines
+mv ${install_dir}/world_boundaries/coastlines/*  ${install_dir}/world_boundaries/
+rmdir  ${install_dir}/world_boundaries/coastlines
 
 
 echo "Add local stuff"
-cp mirror/local/world_bnd* ${instal_dir}/world_boundaries/
+cp mirror/local/world_bnd* ${install_dir}/world_boundaries/
 
 if false; then
     # Old Method copying from local installation
     cp /usr/share/mapnik/world_boundaries/* \
-	${instal_dir}/world_boundaries/
+	${install_dir}/world_boundaries/
 fi
