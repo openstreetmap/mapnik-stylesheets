6206022ebc2a02c8c976d08a212ae6b867d0646f
ldp <ldp@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-04 21:00:31 +0000

    Hopefully fixed it right this time.
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@19871 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- generate_image.py
+++ generate_image.py
@@ -13,7 +13,7 @@
 # increases but note that the text is rendered at a constant
 # pixel size so will appear smaller on a large image.
 
-from mapnik import *
+import mapnik
 import sys, os
 
 if __name__ == "__main__":
@@ -33,17 +33,17 @@ if __name__ == "__main__":
     imgx = 500 * z
     imgy = 1000 * z
 
-    m = Map(imgx,imgy)
-    load_map(m,mapfile)
-    prj = Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over")
-    c0 = prj.forward(Coord(ll[0],ll[1]))
-    c1 = prj.forward(Coord(ll[2],ll[3]))
+    m = mapnik.Map(imgx,imgy)
+    mapnik.load_map(m,mapfile)
+    prj = mapnik.Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over")
+    c0 = prj.forward(mapnik.Coord(ll[0],ll[1]))
+    c1 = prj.forward(mapnik.Coord(ll[2],ll[3]))
     if hasattr(mapnik,'mapnik_version') and mapnik.mapnik_version() >= 800:
-        bbox = Box2d(c0.x,c0.y,c1.x,c1.y)
+        bbox = mapnik.Box2d(c0.x,c0.y,c1.x,c1.y)
     else:
-        bbox = Envelope(c0.x,c0.y,c1.x,c1.y)
+        bbox = mapnik.Envelope(c0.x,c0.y,c1.x,c1.y)
     m.zoom_to_box(bbox)
-    im = Image(imgx,imgy)
-    render(m, im)
+    im = mapnik.Image(imgx,imgy)
+    mapnik.render(m, im)
     view = im.view(0,0,imgx,imgy) # x,y,width,height
     view.save(map_uri,'png')
