73eb4499aee1c4982e40d3b4f15f80fd263c5221
jochen <jochen@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-03-12 17:03:38 +0000

    improved mapnik postgis log analyze script
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@20440 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- utils/analyze_postgis_log.pl
+++ utils/analyze_postgis_log.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
-
+#
+# analyze_postgis_log.pl [MAPFILE] <postgres.log
+#
+# MAPFILE ist the name of the Mapnik stylefile, default 'osm.xml'
+#
 # A script that reads a PostGIS log file AND an osm.xml style file and tries
 # to match the select queries from both, delivering indications about which
 # layers (at which zoom levels) require the longest execution times.
 #
 # prerequisites:
-# * osm.xml in current directory
 # * xmlstarlet installed
 # * Postgres log file on stdin, with lines that look like this:
 #   2009-11-22 06:30:50 CET osm LOG:  duration: 26266.851 ms  execute <unnamed>: SELECT AsBinary("way",'NDR') AS geom,"disused","name","waterway" from (select way,waterway,disused,name,case when tunnel in ('yes','true','1') then 'yes'::text else tunnel end as tunnel from planet_osm_line where waterway IS NOT NULL order by z_order) as water_lines WHERE "way" && SetSRID('BOX3D(-1.428222656250023 53.31774904749086,-1.032714843749977 53.55336278552809)'::box3d,4326)
@@ -19,6 +22,9 @@
 
 # Written by Frederik Ramm <frederik@remote.org>, PD.
 
+use strict;
+use warnings;
+
 use Math::Trig;
 
 my $input;
@@ -26,7 +32,9 @@ my $statement;
 my $duration;
 my $timings = {};
 
-while(1)
+my $mapfile = shift || 'osm.xml';
+
+while (1)
 {
     $input = <>;
     chomp $input;
@@ -40,7 +48,7 @@ while(1)
             # duration given on extra line
             $duration = $1;
         }
-        process_statement($statement, $duration) if defined($statement);
+        process_statement($statement, $duration) if (defined $statement);
         last unless defined($input);
         undef $statement;
         if ($rest =~ /^\S+ \S+ LOG:\s+(duration: (\d+)(\.\d+)? ms\s+)?execute <[^>]*>: (SELECT .*)/)
@@ -58,7 +66,7 @@ while(1)
     }
     else
     {
-        $statement .= $input if defined($statement);
+        $statement .= $input if (defined $statement);
     }
 }
 
@@ -67,7 +75,7 @@ my $potential_originating_layers = findStatements();
 foreach my $ds(sort { $timings->{$b}->{-1} <=> $timings->{$a}->{-1} } keys %$timings)
 {
     print "$ds\n";
-    printf "* potential originating layers: ";
+    printf "* probable originating layers: ";
     my $comma = "";
     foreach my $pol(@{$potential_originating_layers->{$ds}})
     {
@@ -76,13 +84,24 @@ foreach my $ds(sort { $timings->{$b}->{-1} <=> $timings->{$a}->{-1} } keys %$tim
     }
     print "none found" if ($comma eq "");
     print "\n";
-    foreach my $z(sort { $a <=> $b } keys %{$timings->{$ds}})
+    print "  zoom  count  min time  avg time  max time\n";
+    print "  -----------------------------------------\n";
+    foreach my $z (sort { $a <=> $b } keys %{$timings->{$ds}})
     {
         next unless $z >= 0;
-        printf "* zoom %d: %d invocations, average time %5.1fs\n",
-            $z,
-            $timings->{$ds}->{$z}->{count},
-            $timings->{$ds}->{$z}->{sum} / $timings->{$ds}->{$z}->{count} / 1000;
+
+        my $t = $timings->{$ds}->{$z};
+
+        my $count =  $t->{count};
+        my $min   = ($t->{min}//0)           / 1000;
+        my $avg   =  $t->{sum} / $t->{count} / 1000;
+        my $max   =  $t->{max}               / 1000;
+
+        my $warn = '';
+        $warn = '***'   if ($count >  10 && ($min >  10 || $avg >  20 || $max >  30));
+        $warn = '*****' if (                ($min >  60 || $avg >  80 || $max > 100));
+
+        printf "    %2d %6d %8.1fs %8.1fs %8.1fs $warn\n", $z, $count, $min, $avg, $max;
     }
     printf "\n";
 }
@@ -97,6 +116,8 @@ sub process_statement
         my $zoom = guessZoomLevel($left, $bottom, $right, $top);
         $timings->{$datasource}->{$zoom}->{count} ++;
         $timings->{$datasource}->{$zoom}->{sum} += $dur;
+        $timings->{$datasource}->{$zoom}->{min} = $dur if ($dur < ($timings->{$datasource}->{$zoom}->{min} // 999_999_999_999_999));
+        $timings->{$datasource}->{$zoom}->{max} = $dur if ($dur > ($timings->{$datasource}->{$zoom}->{max} // 0));
         # the following line determines the sorting of the final results. 
         # currently the queries that use the longest total time are at the 
         # top but you could also do a "max" calculation here or simply count
@@ -130,7 +151,7 @@ sub guessZoomLevel
     return $zoom;
 }
 
-sub getTileNumber 
+sub getTileNumber
 {
     my ($lat,$lon,$zoom) = @_;
     my $xtile = int(($lon+180)/360 *2**$zoom);
@@ -144,9 +165,11 @@ sub findStatements
     my $result;
 
     # Using xmlstarlet resolves all entities for us...
-    open(STYLE, "xmlstarlet c14n osm.xml|") or die;
+    open(STYLE, "xmlstarlet c14n $mapfile|") or die;
 
     my $styles = {};
+    my $currentlayername;
+    my $where;
 
     while(<STYLE>)
     {
@@ -154,7 +177,6 @@ sub findStatements
         if (/<Style name="(.*)"/)
         {
             $styles->{$1} = { name => $1 };
-            $currentstyle = $styles->{$1};
         }
         elsif (/<Layer\s.*name="([^"]+)"/)
         {
@@ -197,3 +219,4 @@ sub findStatements
     }
     return $result;
 }
+
