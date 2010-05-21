370bacfe620228880d315f12f6ead5c8a644f557
rodo <rodo@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-05-21 14:18:34 +0000

    Fix a little typo
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@21387 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 wget http://tile.openstreetmap.org/world_boundaries-spherical.tgz -O world_boundaries-spherical.tgz
-wget http://tile.openstreetmap.org/processed_p.tar.bz2- O processed_p.tar.bz2
+wget http://tile.openstreetmap.org/processed_p.tar.bz2 -O processed_p.tar.bz2
 wget http://tile.openstreetmap.org/shoreline_300.tar.bz2 -O shoreline_300.tar.bz2
     
 tar xvf world_boundaries-spherical.tgz
 
 if [ -d world_boundaries ]; then
 
-if [ -f processed_p.tar.bz2]; then
-    tar xvf processed_p.tar.bz2
-    mv processed_p.[dis]* world_boundaries/
-else
-    echo 'processed_p.tar.bz2 not present'
-fi
-
-if [ -f shoreline_300.tar.bz2]; then
-    tar xvf shoreline_300.tar.bz2
-    mv shoreline_300.[dis]** world_boundaries/
-fi
+    if [ -f processed_p.tar.bz2 ]; then
+	tar xvf processed_p.tar.bz2
+	mv processed_p.[dis]* world_boundaries/
+    else
+	echo 'processed_p.tar.bz2 not present'
+    fi
+    
+    if [ -f shoreline_300.tar.bz2 ]; then
+	tar xvf shoreline_300.tar.bz2
+	mv shoreline_300.[dis]* world_boundaries/
+    fi
 
 fi
