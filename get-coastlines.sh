e3e78424db707650ae75f8293a2c45e1dc644227
mazdermind <mazdermind@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-07-27 19:32:24 +0000

    added new shapefiles from r21817
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@22456 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -3,21 +3,37 @@
 wget http://tile.openstreetmap.org/world_boundaries-spherical.tgz -O world_boundaries-spherical.tgz
 wget http://tile.openstreetmap.org/processed_p.tar.bz2 -O processed_p.tar.bz2
 wget http://tile.openstreetmap.org/shoreline_300.tar.bz2 -O shoreline_300.tar.bz2
-    
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-populated-places.zip -O 10m-populated-places.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m-admin-0-boundary-lines.zip -O 110m-admin-0-boundary-lines.zip
+
 tar xvf world_boundaries-spherical.tgz
 
 if [ -d world_boundaries ]; then
 
-    if [ -f processed_p.tar.bz2 ]; then
-	tar xvf processed_p.tar.bz2
-	mv processed_p.[dis]* world_boundaries/
-    else
-	echo 'processed_p.tar.bz2 not present'
-    fi
-    
-    if [ -f shoreline_300.tar.bz2 ]; then
-	tar xvf shoreline_300.tar.bz2
-	mv shoreline_300.[dis]* world_boundaries/
-    fi
+	if [ -f processed_p.tar.bz2 ]; then
+		tar xvf processed_p.tar.bz2
+		mv processed_p.[dis]* world_boundaries/
+	else
+		echo 'processed_p.tar.bz2 not present'
+	fi
+
+	if [ -f shoreline_300.tar.bz2 ]; then
+		tar xvf shoreline_300.tar.bz2
+		mv shoreline_300.[dis]* world_boundaries/
+	else
+		echo 'shoreline_300.tar.bz2 not present'
+	fi
+
+	if [ -f 10m-populated-places.zip ]; then
+		unzip 10m-populated-places.zip -d world_boundaries
+	else
+		echo '10m-populated-places.zip not present'
+	fi
+
+	if [ -f 110m-admin-0-boundary-lines.zip ]; then
+		unzip 110m-admin-0-boundary-lines.zip -d world_boundaries
+	else
+		echo '110m-admin-0-boundary-lines.zip not present'
+	fi
 
 fi
