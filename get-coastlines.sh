c318688cf3f33ff63dd9c5bf9941d78036341fae
rodo <rodo@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-08-11 08:48:48 +0000

    add test on executable
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@22615 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+UNZIP=/usr/bin/unzip
+TAR=/bin/tar
+BUNZIP2=/bin/bunzip2
+
+if [ ! -x $UNZIP ]; then
+    echo "unzip is not installed in $UNZIP, it is needed by this script"
+    exit
+fi
+
+if [ ! -x $TAR ]; then
+    echo "tar is not installed in $TAR, it is needed by this script"
+    exit
+fi
+
+if [ ! -x $BUNZIP2 ]; then
+    echo "bunzip2 is not installed in $BUNZIP2, it is needed by this script"
+    exit
+fi
+
+
 wget http://tile.openstreetmap.org/world_boundaries-spherical.tgz -O world_boundaries-spherical.tgz
 wget http://tile.openstreetmap.org/processed_p.tar.bz2 -O processed_p.tar.bz2
 wget http://tile.openstreetmap.org/shoreline_300.tar.bz2 -O shoreline_300.tar.bz2
