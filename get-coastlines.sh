7b04cccc7613a662029bbed760aa18bb11ae5c8b
rodo <rodo@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-10-25 16:54:13 +0000

    Improve binary detection and use
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@23827 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# Copyright (C) 2010 Rodolphe Quiédeville <rodolphe@quiedeville.org> 
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 UNZIP=/usr/bin/unzip
 TAR=/bin/tar
 BUNZIP2=/bin/bunzip2
+WGET=/usr/bin/wget
 
 if [ ! -x $UNZIP ]; then
     echo "unzip is not installed in $UNZIP, it is needed by this script"
@@ -19,39 +27,43 @@ if [ ! -x $BUNZIP2 ]; then
     exit
 fi
 
+if [ ! -x $WGET ]; then
+    echo "wget is not installed in $WGET, it is needed by this script"
+    exit
+fi
 
-wget http://tile.openstreetmap.org/world_boundaries-spherical.tgz -O world_boundaries-spherical.tgz
-wget http://tile.openstreetmap.org/processed_p.tar.bz2 -O processed_p.tar.bz2
-wget http://tile.openstreetmap.org/shoreline_300.tar.bz2 -O shoreline_300.tar.bz2
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-populated-places.zip -O 10m-populated-places.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m-admin-0-boundary-lines.zip -O 110m-admin-0-boundary-lines.zip
+$WGET http://tile.openstreetmap.org/world_boundaries-spherical.tgz -O world_boundaries-spherical.tgz
+$WGET http://tile.openstreetmap.org/processed_p.tar.bz2 -O processed_p.tar.bz2
+$WGET http://tile.openstreetmap.org/shoreline_300.tar.bz2 -O shoreline_300.tar.bz2
+$WGET http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-populated-places.zip -O 10m-populated-places.zip
+$WGET http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m-admin-0-boundary-lines.zip -O 110m-admin-0-boundary-lines.zip
 
-tar xvf world_boundaries-spherical.tgz
+$TAR xvf world_boundaries-spherical.tgz
 
 if [ -d world_boundaries ]; then
 
 	if [ -f processed_p.tar.bz2 ]; then
-		tar xvf processed_p.tar.bz2
+		$TAR xvf processed_p.tar.bz2
 		mv processed_p.[dis]* world_boundaries/
 	else
 		echo 'processed_p.tar.bz2 not present'
 	fi
 
 	if [ -f shoreline_300.tar.bz2 ]; then
-		tar xvf shoreline_300.tar.bz2
+		$TAR xvf shoreline_300.tar.bz2
 		mv shoreline_300.[dis]* world_boundaries/
 	else
 		echo 'shoreline_300.tar.bz2 not present'
 	fi
 
 	if [ -f 10m-populated-places.zip ]; then
-		unzip 10m-populated-places.zip -d world_boundaries
+		$UNZIP 10m-populated-places.zip -d world_boundaries
 	else
 		echo '10m-populated-places.zip not present'
 	fi
 
 	if [ -f 110m-admin-0-boundary-lines.zip ]; then
-		unzip 110m-admin-0-boundary-lines.zip -d world_boundaries
+		$UNZIP 110m-admin-0-boundary-lines.zip -d world_boundaries
 	else
 		echo '110m-admin-0-boundary-lines.zip not present'
 	fi
