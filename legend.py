28d5e2239e343c3189ef5bce181eed4f92e8f4ee
bnesbitt <bnesbitt@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2013-06-20 18:51:59 +0000

    Tweak so it at least runs and produces output, just to see what it does.
    git-svn-id: http://svn.openstreetmap.org/applications/rendering/mapnik@29676 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- legend.py
+++ legend.py
@@ -3,7 +3,7 @@
 """A simple script to help visualize the Mapnik stylesheet. This file puts
    out poly/line symbols into an HTML file, with their associated filters.
    Not particularly complex at the moment, but a useful visualization tool if
-   you're hacking on the osm.xml file. Works against the osm-template.xml
+   you're hacking on the osm.xml file. Works against the osm.xml
    at the moment."""
 
 __author__ = "Christopher Schmidt"
@@ -13,16 +13,19 @@ __license__ = "Public Domain"
 import xml.dom.minidom as m
 
 def run():
-    """Parses the osm-template.xml file, and `print`s out an HTML table.""" 
-    doc = m.parse("osm-template.xml")
+    """Parses the osm.xml file, and write an HTML table.""" 
+    #doc = m.parse("osm-template.xml")
+    doc = m.parse("osm.xml")
     styles = doc.getElementsByTagName("Style")
     for style in styles:
         table_started = False
         for r in style.getElementsByTagName("Rule"):
+
             filters = r.getElementsByTagName("Filter")
             text = ""
             if len(filters):
                 text = filters[0].firstChild.nodeValue
+
             polys = r.getElementsByTagName("PolygonSymbolizer")     
             poly_text = [] 
             poly_style = {}
@@ -30,13 +33,15 @@ def run():
                 css = polys[0].getElementsByTagName("CssParameter")
                 for c in css:
                     poly_style[ c.getAttribute("name") ] = c.firstChild.nodeValue
+
             pfill = r.getElementsByTagName("PolygonPatternSymbolizer")
             if len(pfill):
                 pfill = pfill[0]
-                poly_style['fill'] = '<img src="%s" title="%s"/>' % (pfill.getAttribute("file").replace("%SYMBOLS_DIR%", "symbols"), pfill.getAttribute("file").replace("%SYMBOLS_DIR%", "symbols"))
+                poly_style['fill'] = '<img src="symbols/%s" title="%s"/>' % (pfill.getAttribute("file"), pfill.getAttribute("file"))
             for key, value in poly_style.items():
                 poly_text.append("%s: %s" % (key, value))
             poly_text = "<br />".join(poly_text)
+
             lines = r.getElementsByTagName("LineSymbolizer")     
             line_text = [] 
             line_style = {}
@@ -47,9 +52,9 @@ def run():
                 for key, value in line_style.items():
                     line_text.append("%s: %s" % (key, value))
             line_text = "<br />".join(line_text)
+
             if text and (poly_text or line_text):
                 if not table_started:
-    
                     print "<h2>%s</h2>" % style.getAttribute("name").title()
                     print "<table><tr><td>filter</td><td>poly</td><td>stroke</td></tr>"
                     table_started = True
